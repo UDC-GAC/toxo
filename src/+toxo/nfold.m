19be9815fd9ef73c8ac548ed16de7522e7f9f16d
Christian Ponte <christian.ponte@udc.es>
2018-11-26 11:11:38 +0100

    Add documentation to function nfold

--- src/+toxo/nfold.m
+++ src/+toxo/nfold.m
@@ -1,6 +1,8 @@
 function A = nfold(vector, n)
-%NFOLD Summary of this function goes here
-%   https://www.mathworks.com/matlabcentral/answers/282777-n-fold-cartesian-product
+%NFOLD N-times cartesian product of a vector with itself.
+%   Source code provided by John D'Errico at:
+%   https://mathworks.com/matlabcentral/answers/282777-n-fold-cartesian-product#answer_220945
+
 s = repmat({vector}, 1, n);
 [A{n:-1:1}] = ndgrid(s{:});
 A = reshape(cat(n, A{:}), [], n);
