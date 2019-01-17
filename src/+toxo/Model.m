79857b1f17b9df64a08fdcb4f5b1a30301f0869e
Christian Ponte <christian.ponte@udc.es>
2019-01-17 10:31:50 +0100

    Rewrite find_max_heritability method

--- src/+toxo/Model.m
+++ src/+toxo/Model.m
@@ -1,7 +1,7 @@
 classdef Model
-    %MODEL Abstract representation of a penetrance model as a function
-    % of two variables. This class provides methods to obtain numeric
-    % penetrance tables derived from the parametric representation given as
+    %MODEL Abstract representation of a penetrance model as a function of
+    % two variables. This class provides methods to obtain numeric pene-
+    % trance tables derived from the parametric representation given as
     % input.
     
     properties
@@ -11,31 +11,26 @@ classdef Model
         variables     % List of all variables contained in all symbolic expressions
     end
     
-    methods (Access = private, Static = true)
-        function [c, ceq] = hconstraint(pt, h)
-            c = [];
-            ceq = pt.heritability() - h;
-        end
-        
-        function [c,ceq] = pconstraint(pt, p)
-            c = [];
-            ceq = pt.prevalence() - p;
+    methods (Access = private)
+        function p = prevalence(obj, maf)
+            gp = toxo.genotype_probabilities(maf, obj.order);
+            p = sum(obj.penetrances .* gp);
         end
     end
     
     methods
         function obj = Model(path)
-            %MODEL Construct an instance of this class from the given model.
+            %MODEL Construct an instance of this class from the given
+            % model.
+            % 
             % M = MODEL(P) reads the model from its text representation in
-            % file path P.
-            %
-            % The input model must be formatted as a plain CSV, with each
-            % line of the file P corresponding to a row of the model. The 
-            % rows are made of the genotype definition and the probability
-            % associated with the given genotype, separated by a coma.
-            % Probability is expressed as a function of two variables.
-            % Empty lines, as well as lines starting with '#' will be
-            % ignored.
+            % file path P. The input model must be formatted as a plain
+            % CSV, with eachline of the file P corresponding to a row of 
+            % the model. The rows are made of the genotype definition and
+            % the probability associated with the given genotype, separated
+            % by a coma. Probability is expressed as a function of two 
+            % variables, represented as alphabetical characters. Empty
+            % lines, as well as lines starting with '#' will be ignored.
             
             [~, obj.name, ~] = fileparts(path);
             fid = fopen(path, 'r');
@@ -49,13 +44,13 @@ classdef Model
         end
         
         function pt = find_max_prevalence(obj, maf, h)
-            %FIND_MAX_PREVALENCE Compute the penetrance table of the model with the maximum admissible prevalence given its MAF and heritability.
-            % PT = FIND_MAX_PREVALENCE(MAF, H) returns a PT instance with the
-            % maximum admissible prevalence given the MAF and heritability
-            % restraints.
-            % Finding the maximum prevalence requires solving a minimization
-            % problem, since the resulting equation system involves
-            % non-polynomial terms and can not be solved analytically.
+            % FIND_MAX_PREVALENCE Compute the penetrance table of the model with the maximum admissible prevalence given its MAF and heritability.
+            %   PT = FIND_MAX_PREVALENCE(MAF, H) returns a PT instance with the
+            %   maximum admissible prevalence given the MAF and heritability
+            %   restraints.
+            %   Finding the maximum prevalence requires solving a minimization
+            %   problem, since the resulting equation system involves
+            %   non-polynomial terms and can not be solved analytically.
             
             highest_degree_monomial = obj.symbolic_penetrances(end);
             bfunction = matlabFunction(rhs(isolate(highest_degree_monomial == 1, sym('b'))));
@@ -73,28 +68,24 @@ classdef Model
         end
         
         function pt = find_max_heritability(obj, maf, p)
-            % FIND_MAX_HERITABILITY Compute the penetrance table of the model with the maximum admissible heritability given its MAF and prevalence.
-            %   PT = FIND_MAX_PREVALENCE(MAF, H) returns a PT instance with the
-            %   maximum admissible heritability given the MAF and prevalence
-            %   restraints.
-            %   Finding the maximum heritability requires solving a
-            %   minimization problem, since the resulting equation system
-            %   involves non-polynomial terms and can not be solved
-            %   analytically.
+            %FIND_MAX_HERITABILITY Calculate the penetrance table(s) of the
+            % model with the maximum admissible heritability given its MAF
+            % and prevalence.
+            % 
+            % PT = FIND_MAX_HERITABILITY(MAF, P) returns a list of pene-
+            % trance tables (as PTable objects) that maximize the heri-
+            % tability given the MAF and prevalence constraints.
             
-            highest_degree_monomial = obj.symbolic_penetrances(end);
-            afunction = matlabFunction(rhs(isolate(highest_degree_monomial == 1, sym('a'))));
-            lb = 0;
-            ub = inf;
-            x0 = 0;
-            nonlcon = @(x) toxo.Model.pconstraint(toxo.PT(obj, maf, afunction(x), x), p);
-            options = optimoptions('fmincon', 'Display', 'none', 'StepTolerance', 1e-120, 'ConstraintTolerance', 1e-3);
-            [beta, alpha, exitflag, output] = fmincon(afunction, x0, [], [], [], [], lb, ub, nonlcon, options);
-            if exitflag < 0
-                ME = MException("Model:no_solution", output.message);
-                throw(ME);
-            end
-            pt = toxo.PT(obj, maf, alpha, beta);
+            p_constraint = obj.prevalence(maf) == p;
+            [~, i] = max(subs(obj.penetrances, obj.variables, randi(2^53-1, size(obj.variables))));
+            max_poly = obj.penetrances(i);
+            S = solve([p_constraint, max_poly == 1], obj.variables);
+            x = vpa(S.x);
+            y = vpa(S.y);
+            solutions = arrayfun(@(z, t) isreal(z) && isreal(t) && z >= 0 && t >= 0, x, y);
+            x = x(solutions);
+            y = y(solutions);
+            pt = arrayfun(@(z, t) toxo.PT(obj, maf, z, t), x, y);
         end
     end
 end
