7d4ef2681d1897a2540aafd6f2f3f0673938d4fe
Christian Ponte <christian.ponte@udc.es>
2019-01-18 12:16:40 +0100

    Modify genotype_probabilities to use symbolic expressions for fractions, avoiding losses in decimal precision

--- src/+toxo/genotype_probabilities.m
+++ src/+toxo/genotype_probabilities.m
@@ -1,6 +1,7 @@
 function [p] = genotype_probabilities(maf, order)
 % GENOTYPE_PROBABILITIES Compute the probabilities associated with each genotype for a given order and MAF.
-
-p = prod(toxo.nfold([(1 - maf)^2, 2 * maf * (1 - maf), maf^2], order), 2);
+m = sym(maf);
+M = 1 - m;
+p = prod(toxo.nfold([M^2, 2 * M * m, m^2], order), 2);
 end
 
