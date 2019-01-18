1ee0c18d393304fddf41a28bf3a7fb2edea447f2
Christian Ponte <christian.ponte@udc.es>
2019-01-18 13:30:44 +0100

    Update generate_models sample script with Model and PTable changes

--- generate_models.m
+++ generate_models.m
@@ -1,35 +1,35 @@
 %% Import Toxo library
 addpath('src/');
-import toxo.*;
 
 %% Penetrance table generation
-% Read models from models/ folder
+% Read all the models from models/ folder
 models = {};
 for m = dir('models')'
     if ~ m.isdir
-        models{end + 1} = Model(fullfile(m.folder, m.name));
+        models{end + 1} = toxo.Model(fullfile(m.folder, m.name));
     end
 end
 
-% Create MAF and heritabilities to test
+% Create a list of MAFs and heritabilities to test
 maf = [0.1, 0.25, 0.4];
 h = [0.1, 0.25, 0.5, 0.8];
 
-% Find penetrance tables and write the results into files
+% Create the output folder
 output_folder = "output\";
 if ~ isfolder(output_folder)
     mkdir(output_folder);
 end
 
+% Find the associated penetrance tables and write the results into files
 for m = models
     for i = maf
         for j = h
             try
                 pt = m{:}.find_max_prevalence(i, j);
                 file_name = sprintf("%s_%.2f_h%.2f.txt", m{:}.name, i, j);
-                pt.write(fullfile(output_folder, file_name), PT.format_gametes);
+                pt.write(fullfile(output_folder, file_name), toxo.PTable.format_gametes);
             catch ME
-                % disp(ME.message);
+                disp(ME.message);
                 warning("Unable to generate model %s with MAF %f and heritability %f.\n", m{:}.name, i, j);
                 continue;
             end
